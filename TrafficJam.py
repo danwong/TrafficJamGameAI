f43e2b7615946e5d6ba1dc1072a63c2beb2be2e0
Daniel Wong <dwong56@calpoly.edu>
2017-04-07 23:19:50 -0700

    Started to add logic of move

--- TrafficJam.py
+++ TrafficJam.py
@@ -14,3 +14,20 @@ def isFinished(board):
 	if board == ['<', '<', '<', '<', '<', ' ', '>', '>', '>', '>', '>']:
 		return True
 	return False
+
+def move(board, input):
+	left = '<'
+	right = '>'
+	if (input == left):
+		if (board[input-1] == ' '):
+			board[input] = ' '
+			board[input-1] = '<'
+			return True
+		else if (board[input-1] == '>'):
+			if board[input-2] == ' '
+				board[input-2] = '<'
+			return True
+		return False
+	else if (input == right):
+		
+
